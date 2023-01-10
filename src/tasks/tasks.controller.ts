50d3048d5945b39baafc2e0221cb8f32db78b49a
amirm8170 <amir_mabood@yahoo.com>
2023-01-10 20:57:10 +0300

    add validator

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,17 @@
-import { Task } from './task.model';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { updateTaskStatusDto } from './DTO/update-task-status.dto';
+import { filterDto } from './DTO/search-task.dto';
+import { createTaskDto } from './DTO/create-task.dto';
+import { Task, TaskStatus } from './task.model';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -9,12 +21,35 @@ export class TasksController {
   getAllTasks() {
     return this.TasksService.getAllTasks();
   }
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.TasksService.getTaskById(id);
+  }
+  @Get()
+  searchTaskDto(@Query() filterDto: filterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.TasksService.searchTaskDto(filterDto);
+    } else {
+      return this.TasksService.getAllTasks();
+    }
+  }
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): string {
+    this.TasksService.deleteTaskById(id);
+    return 'deleted';
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body() updateTaskStatusDto: updateTaskStatusDto,
+  ): Task {
+    const { status } = updateTaskStatusDto;
+    return this.TasksService.updateTaskById(id, status);
+  }
 
   @Post()
-  createTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ) {
-    return this.TasksService.createTask(title, description);
+  createTask(@Body() createTaskDto: createTaskDto): Task {
+    return this.TasksService.createTask(createTaskDto);
   }
 }
